40c4534d662ae3bd58720a0d4ab293a0aa862f32
Jason Nam <contact@jasonnam.com>
2018-08-04 21:18:42 +0700

    Add Customizable

---
+++ Sources/Steroid/Customizable.swift
@@ -0,0 +1,43 @@
+//
+//  Customizable.swift
+//  Steroid
+//
+//  Copyright (c) 2018 Jason Nam (https://jasonnam.com)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+// Inspired By
+// https://github.com/devxoul/Then
+// https://github.com/jlandon/Alexandria/blob/master/Sources/NSObject%2BCustomizable.swift
+
+import Foundation
+
+public protocol Customizable {}
+
+public extension Customizable {
+
+    @discardableResult
+    public func customize(_ customize: (Self) -> Void) -> Self {
+        customize(self)
+        return self
+    }
+}
+
+extension NSObject: Customizable {}
