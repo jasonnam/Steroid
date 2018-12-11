d0957718f7b02de904efa82f4e8c36b5241ae51f
Jason Nam <contact@jasonnam.com>
2018-12-11 15:19:26 +0700

    Add UICollectionView extension

---
+++ Sources/Steroid/UICollectionView/UICollectionView+Dequeue.swift
@@ -0,0 +1,42 @@
+//
+//  UICollectionView+Dequeue.swift
+//  Steroid
+//
+//  Copyright (c) 2018 Jason Nam (https://jasonnam.com)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import UIKit
+
+public extension UICollectionView {
+
+    public func register<T: UICollectionViewCell>(_ classType: T.Type) {
+        register(classType, forCellWithReuseIdentifier: .init(describing: T.self))
+    }
+
+    public func registerNib<T: UICollectionViewCell>(_ classType: T.Type) {
+        let className = String(describing: T.self)
+        register(.init(nibName: className, bundle: nil), forCellWithReuseIdentifier: className)
+    }
+
+    public func dequeue<T: UICollectionViewCell>(_ classType: T.Type, for indexPath: IndexPath) -> T {
+        return dequeueReusableCell(withReuseIdentifier: .init(describing: T.self), for: indexPath) as! T
+    }
+}
